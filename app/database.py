1f472b2383675451ea70a0aece4ee1286586ad38
Priyadharshiniprabhu <priyadharshini.p246736@gmail.com>
2026-08-28 10:13:59 +0530

    Debug Database

--- app/database.py
+++ app/database.py
@@ -2,7 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "postgresql://postgres:PriVic1522@localhost:5432/TaskManagement"
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL =", DATABASE_URL)
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is missing")
+
+engine = create_engine(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
